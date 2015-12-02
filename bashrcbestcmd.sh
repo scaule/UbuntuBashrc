18cd9ed7f79fee4447b6b99c8b9c1cf5c4f32054
sicau <sicau@smile.fr>
2015-12-02 16:19:59 +0100

    add bashrc best commmand

---
+++ bashrcbestcmd.sh
@@ -0,0 +1,9 @@
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias next="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next"
+alias previous="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous"
+alias prev="previous;previous"
+alias pp="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause"
+alias poney='sudo $(history -p \!\!)'
